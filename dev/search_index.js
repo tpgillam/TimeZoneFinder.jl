b96964af51f44982f6ad4d6fe6a6941630e8e3ab
Documenter.jl <documenter@juliadocs.github.io>
2022-08-21 20:04:38 +0000

    build based on 5bd10e1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TimeZoneFinder","category":"page"},{"location":"#TimeZoneFinder","page":"Home","title":"TimeZoneFinder","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TimeZoneFinder.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TimeZoneFinder]","category":"page"}]
+}
