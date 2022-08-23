d02cf120ffbb306d856458863ecb44af70cbaffd
Tom Gillam <tpgillam@googlemail.com>
2022-08-23 12:28:28 +0100

    Cache binary version of timezone data

--- src/TimeZoneFinder.jl
+++ src/TimeZoneFinder.jl
@@ -6,6 +6,9 @@ using JSON3
 using LazyArtifacts
 using Memoize
 using Meshes
+using Pkg.TOML
+using Scratch
+using Serialization
 using TimeZones
 
 function _get_points(coord_list)::Vector{Point{2,Float64}}
@@ -28,7 +31,10 @@ function _get_shape(geometry)
     end
 end
 
-@memoize function load_data(release::AbstractString)
+"""
+Generate the timezone map data from the artifact identified by `release`.
+"""
+function generate_data(release::AbstractString)
     artifact_name = "timezone-boundary-builder-$release"
     dir = LazyArtifacts.@artifact_str(artifact_name)
     obj = open(JSON3.read, joinpath(dir, "combined-with-oceans.json"))
@@ -36,7 +42,6 @@ end
     # Vectors that will be populated in the loop below.
     shapes = []
     tzs = []
-
     foreach(obj[:features]) do feature
         # Note: Etc/<Stuff> timezones count as LEGACY timezones. These are used in the
         #   oceans, so for these purposes we allow them.
@@ -57,7 +62,52 @@ end
     shapes = identity.(shapes)
     tzs = identity.(tzs)
 
+    # Package data into a namedtuple.
     return (; shapes, tzs)
+end
+
+"""
+    _scratch_dir(release)
+
+Get the scratch directory path in which the serialized mapping data will be kept.
+"""
+function _scratch_dir(release::AbstractString)
+    # The scratch directory should be different for different package versions, since we
+    # may generate data in a different format.
+    pkg_version = VersionNumber(
+        TOML.parsefile(joinpath(dirname(@__DIR__), "Project.toml"))["version"]
+    )
+
+    # It should also be different for different Julia versions, since the serialisation
+    # protocol may change.
+    julia_version = string(VERSION)
+
+    scratch_name = "$(release)-$(pkg_version)-$(julia_version)"
+    return @get_scratch!(scratch_name)
+end
+
+"""Serialized data file path for this version."""
+_cache_path(release::AbstractString) = joinpath(_scratch_dir(release), "data.bin")
+
+"""
+    load_data(release)
+
+Load timezone map data for `release`.
+
+This is memoized, such that the data is only read from disk once within the lifetime of the
+Julia process.
+"""
+@memoize function load_data(release::AbstractString)
+    # Read data from the cache path if it exists, otherwise generate from the artifact, and
+    # cache.
+    path = _cache_path(release)
+    return if isfile(path)
+        deserialize(path)
+    else
+        data = generate_data(release)
+        serialize(path, data)
+        data
+    end
 end
 
 """
@@ -73,9 +123,7 @@ Europe/Berlin (UTC+1/UTC+2)
 Returns a `TimeZone` instance if `latitude` and `longitude` correspond to a known timezone,
 otherwise `nothing` is returned.
 """
-function timezone_at(
-    latitude::Real, longitude::Real; release::AbstractString="2021c"
-)
+function timezone_at(latitude::Real, longitude::Real; release::AbstractString="2021c")
     data = load_data(release)
     p = Point{2,Float64}(longitude, latitude)
     # This is an unintelligent linear search through all polygons. There is much room for
