c24ccb8c5ff387d028327c33cfe7dfcfd1f9ea9a
Tom Gillam <tpgillam@googlemail.com>
2022-12-02 23:24:14 +0000

    Get tzdata version from TimeZones

--- src/TimeZoneFinder.jl
+++ src/TimeZoneFinder.jl
@@ -11,8 +11,6 @@ using Scratch
 using Serialization
 using TimeZones
 
-const LATEST_RELEASE = "2022f"
-
 function _get_points(coord_list)::Vector{Point{2,Float64}}
     return [Point(Float64(x[1]), Float64(x[2])) for x in coord_list]
 end
@@ -53,10 +51,10 @@ function Base.in(point::Point, bpa::BoundedPolyArea)
 end
 
 """
-Generate the timezone map data from the artifact identified by `release`.
+Generate the timezone map data from the artifact identified by `version`.
 """
-function generate_data(release::AbstractString)
-    artifact_name = "timezone-boundary-builder-$release"
+function generate_data(version::AbstractString)
+    artifact_name = "timezone-boundary-builder-$version"
     dir = LazyArtifacts.@artifact_str(artifact_name)
     obj = open(JSON3.read, joinpath(dir, "combined-with-oceans.json"))
 
@@ -83,11 +81,11 @@ function generate_data(release::AbstractString)
 end
 
 """
-    _scratch_dir(release)
+    _scratch_dir(version)
 
 Get the scratch directory path in which the serialized mapping data will be kept.
 """
-function _scratch_dir(release::AbstractString)
+function _scratch_dir(version::AbstractString)
     # The scratch directory should be different for different package versions, since we
     # may generate data in a different format.
     pkg_version = VersionNumber(
@@ -98,36 +96,36 @@ function _scratch_dir(release::AbstractString)
     # protocol may change.
     julia_version = string(VERSION)
 
-    scratch_name = "$(release)-$(pkg_version)-$(julia_version)"
+    scratch_name = "$(version)-$(pkg_version)-$(julia_version)"
     return @get_scratch!(scratch_name)
 end
 
 """Serialized data file path for this version."""
-_cache_path(release::AbstractString) = joinpath(_scratch_dir(release), "data.bin")
+_cache_path(version::AbstractString) = joinpath(_scratch_dir(version), "data.bin")
 
 """
-    load_data(release)
+    load_data(version)
 
-Load timezone map data for `release`.
+Load timezone map data for `version`.
 
 This is memoized, such that the data is only read from disk once within the lifetime of the
 Julia process.
 """
-@memoize function load_data(release::AbstractString)
+@memoize function load_data(version::AbstractString)
     # Read data from the cache path if it exists, otherwise generate from the artifact, and
     # cache.
-    path = _cache_path(release)
+    path = _cache_path(version)
     return if isfile(path)
         deserialize(path)
     else
-        data = generate_data(release)
+        data = generate_data(version)
         serialize(path, data)
         data
     end
 end
 
 """
-    timezone_at(latitude, longitude; release=LATEST_RELEASE)
+    timezone_at(latitude, longitude)
 
 Get the timezone at the given `latitude` and `longitude`.
 
@@ -136,18 +134,15 @@ julia> timezone_at(52.5061, 13.358)
 Europe/Berlin (UTC+1/UTC+2)
 ```
 
-The optional `release` argument can be used to specify a different version of the
-[timezone-boundary-builder](https://github.com/evansiroky/timezone-boundary-builder) dataset
-to use. By default the latest release will be used, which will be the desired setting for
-common use.
+!!! note
+    The library always uses the same version of tzdata currently used by `TimeZones`.
 
 Returns a `TimeZone` instance if `latitude` and `longitude` correspond to a known timezone,
 otherwise `nothing` is returned.
 """
-function timezone_at(
-    latitude::Real, longitude::Real; release::AbstractString=LATEST_RELEASE
-)
-    data = load_data(release)
+function timezone_at(latitude::Real, longitude::Real)
+    version = TimeZones.TZData.tzdata_version()
+    data = load_data(version)
     p = Point{2,Float64}(longitude, latitude)
     # This is an unintelligent linear search through all polygons. There is much room for
     # improvement by building a spatial index.
