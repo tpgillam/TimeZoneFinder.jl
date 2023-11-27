03d1aebce0a209472a4a42557f87e6df189afeb3
Tom Gillam <tpgillam@googlemail.com>
2023-11-27 11:57:38 +0000

    Format

--- docs/make.jl
+++ docs/make.jl
@@ -14,13 +14,8 @@ makedocs(;
         edit_link="main",
         assets=String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages=["Home" => "index.md"],
     checkdocs=:exports,
 )
 
-deploydocs(;
-    repo="github.com/tpgillam/TimeZoneFinder.jl",
-    devbranch="main",
-)
+deploydocs(; repo="github.com/tpgillam/TimeZoneFinder.jl", devbranch="main")
